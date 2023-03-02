7eb8fa58c093a9fb788893686138e71fd2760229
balaji-githubstore <balaji0017@gmail.com>
2023-03-02 12:29:06 +0530

    Added Fixtures with Function scope for brower config

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from assertpy import assert_that
@@ -5,20 +6,21 @@ from selenium.webdriver.common.by import By
 
 
 class TestLoginUI:
-    def test_title(self):
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
         serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        driver = webdriver.Chrome(service=serv_driver)
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_title = driver.title
+        self.driver = webdriver.Chrome(service=serv_driver)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_title(self):
+        actual_title = self.driver.title
         assert_that("OrangeHRM").is_equal_to(actual_title)
 
     def test_header(self):
-        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        driver = webdriver.Chrome(service=serv_driver)
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_header = driver.find_element(By.XPATH, "//h5").text
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
         assert_that("Login").is_equal_to(actual_header)
