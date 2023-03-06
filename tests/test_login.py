9cc848352ecbca392815aebaf5a5510cb1c44bee
balaji-githubstore <balaji0017@gmail.com>
2023-03-06 11:38:20 +0530

    Added WebDriverWrapper for browser config

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,9 +5,10 @@ from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
-class TestLogin:
+class WebDriverWrapper:
     @pytest.fixture(scope="function", autouse=True)
     def browser_config(self):
+
         serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
         self.driver = webdriver.Chrome(service=serv_driver)
         self.driver.maximize_window()
@@ -16,25 +17,17 @@ class TestLogin:
         yield
         self.driver.quit()
 
+
+class TestLogin(WebDriverWrapper):
     def test_valid_login(self):
-        self.driver.find_element(By.NAME,"username").send_keys("Admin")
-        self.driver.find_element(By.NAME,"password").send_keys("admin123")
-        self.driver.find_element(By.XPATH,"//button[normalize-space()='Login']").click()
-        actual_text=self.driver.find_element(By.XPATH,"//h6[normalize-space()='Dashboard']").text
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
         assert_that("Dashboard").is_equal_to(actual_text)
 
 
-class TestLoginUI:
-    @pytest.fixture(scope="function", autouse=True)
-    def browser_config(self):
-        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        self.driver = webdriver.Chrome(service=serv_driver)
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(20)
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        yield
-        self.driver.quit()
-
+class TestLoginUI(WebDriverWrapper):
     def test_title(self):
         actual_title = self.driver.title
         assert_that("OrangeHRM").is_equal_to(actual_title)
