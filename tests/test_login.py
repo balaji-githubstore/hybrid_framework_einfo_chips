25afa4c5cf344aaeaffb1640787bec78a1679dc5
balaji-githubstore <balaji0017@gmail.com>
2023-03-06 11:16:15 +0530

    Added Valid Login Test

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,10 +5,28 @@ from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
-class TestLoginUI:
-
+class TestLogin:
     @pytest.fixture(scope="function", autouse=True)
-    def setup(self):
+    def browser_config(self):
+        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
+        self.driver = webdriver.Chrome(service=serv_driver)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME,"username").send_keys("Admin")
+        self.driver.find_element(By.NAME,"password").send_keys("admin123")
+        self.driver.find_element(By.XPATH,"//button[normalize-space()='Login']").click()
+        actual_text=self.driver.find_element(By.XPATH,"//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
+
+
+class TestLoginUI:
+    @pytest.fixture(scope="function", autouse=True)
+    def browser_config(self):
         serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
         self.driver = webdriver.Chrome(service=serv_driver)
         self.driver.maximize_window()
