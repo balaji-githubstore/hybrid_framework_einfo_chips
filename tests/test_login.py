25e82f2cbb4c79d00b33f50039f5b52491606ab7
balaji-githubstore <balaji0017@gmail.com>
2023-03-07 11:01:36 +0530

    Added @pytest.mark.parametrize for Invalid Login Test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import pytest
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
@@ -13,13 +14,17 @@ class TestLogin(WebDriverWrapper):
         actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
         assert_that("Dashboard").is_equal_to(actual_text)
 
-    def test_invalid_login(self):
-        self.driver.find_element(By.NAME, "username").send_keys("john")
-        self.driver.find_element(By.NAME, "password").send_keys("john123")
+    @pytest.mark.parametrize("username, password, expected_error", [
+        ("saul", "saul123", "Invalid credentials"),
+        ("kim", "kim123", "Invalid credentials"),
+        ("john", "john123", "Invalid credentials")
+    ])
+    def test_invalid_login(self, username, password, expected_error):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
         actual_error = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Invalid')]").text
-        assert_that("Invalid credentials").is_equal_to(actual_error)
-
+        assert_that(expected_error).is_equal_to(actual_error)
 
 class TestLoginUI(WebDriverWrapper):
     def test_title(self):
@@ -29,3 +34,9 @@ class TestLoginUI(WebDriverWrapper):
     def test_header(self):
         actual_header = self.driver.find_element(By.XPATH, "//h5").text
         assert_that("Login").is_equal_to(actual_header)
+
+    def test_login_placeholders(self):
+        actual_username_placeholder = self.driver.find_element(By.NAME, "username").get_attribute("placeholder")
+        actual_password_placeholder = self.driver.find_element(By.NAME, "password").get_attribute("placeholder")
+        assert_that("Username").is_equal_to(actual_username_placeholder)
+        assert_that("Password").is_equal_to(actual_password_placeholder)
