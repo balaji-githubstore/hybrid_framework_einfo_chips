6d4d9de202c9b84fe1cfb5903a047a76e7722f0d
balaji-githubstore <balaji0017@gmail.com>
2023-03-07 11:52:20 +0530

    Created data_source.py for data handling

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,7 @@ from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 from base.webdriver_listener import WebDriverWrapper
+from utilities import data_source
 
 
 class TestLogin(WebDriverWrapper):
@@ -14,17 +15,14 @@ class TestLogin(WebDriverWrapper):
         actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
         assert_that("Dashboard").is_equal_to(actual_text)
 
-    @pytest.mark.parametrize("username, password, expected_error", [
-        ("saul", "saul123", "Invalid credentials"),
-        ("kim", "kim123", "Invalid credentials"),
-        ("john", "john123", "Invalid credentials")
-    ])
+    @pytest.mark.parametrize("username, password, expected_error", data_source.test_invalid_login_data)
     def test_invalid_login(self, username, password, expected_error):
         self.driver.find_element(By.NAME, "username").send_keys(username)
         self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
         actual_error = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Invalid')]").text
         assert_that(expected_error).is_equal_to(actual_error)
+
 
 class TestLoginUI(WebDriverWrapper):
     def test_title(self):
---
+++ utilities/data_source.py
@@ -0,0 +1,5 @@
+test_invalid_login_data = [
+    ("saul", "saul123", "Invalid credentials"),
+    ("kim", "kim123", "Invalid credentials"),
+    ("john", "john123", "Invalid credentials")
+]
