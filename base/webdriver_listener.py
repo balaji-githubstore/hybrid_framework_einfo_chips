231a007dda45c1e4a0f2156be309742d98079dbf
balaji-githubstore <balaji0017@gmail.com>
2023-03-10 12:19:17 +0530

    Cross Browser Config with Driver Config

--- base/webdriver_listener.py
+++ base/webdriver_listener.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EgdeService
 
 
 class WebDriverWrapper:
@@ -8,8 +9,19 @@ class WebDriverWrapper:
 
     @pytest.fixture(scope="function", autouse=True)
     def browser_config(self):
-        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        self.driver = webdriver.Chrome(service=serv_driver)
+        browser_name = "edge"
+
+        if browser_name == "edge":
+            # srv = EgdeService(executable_path=r"C:\Mine\Software\msedgedriver.exe")
+            # self.driver = webdriver.Edge(service=srv)
+            self.driver = webdriver.Edge()
+        elif browser_name == "ff":
+            self.driver = webdriver.Firefox()
+        else:
+            # srv = ChromeService(executable_path=C:\Mine\Software\chromedriver.exe)
+            # self.driver = webdriver.Chrome(srv)
+            self.driver = webdriver.Chrome()
+
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
