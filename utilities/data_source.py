191432a606a37e2e1e6fb6f4f852aee46d418a1d
balaji-githubstore <balaji0017@gmail.com>
2023-03-10 12:18:19 +0530

    Added Upload Profile Test with Excel connect

--- utilities/data_source.py
+++ utilities/data_source.py
@@ -17,6 +17,10 @@ from utilities import read_utils
 # ]
 
 
-test_invalid_login_data=read_utils.get_csv_as_list("../test_data/test_invalid_login_data.csv")
+test_invalid_login_data = read_utils.get_csv_as_list("../test_data/test_invalid_login_data.csv")
 
-test_add_valid_employee_data=read_utils.get_sheet_as_list("../test_data/orange_test_data.xlsx","test_add_valid_employee")
+test_add_valid_employee_data = read_utils.get_sheet_as_list("../test_data/orange_test_data.xlsx",
+                                                            "test_add_valid_employee")
+
+test_invalid_profile_upload_data = read_utils.get_sheet_as_list("../test_data/orange_test_data.xlsx",
+                                                                "test_invalid_profile_upload")
