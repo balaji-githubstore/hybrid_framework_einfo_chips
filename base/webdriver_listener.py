45aec9a5c31edf7bb37498724279ff974229650a
balaji-githubstore <balaji0017@gmail.com>
2023-03-10 12:21:40 +0530

    Headless Mode

--- base/webdriver_listener.py
+++ base/webdriver_listener.py
@@ -10,17 +10,18 @@ class WebDriverWrapper:
     @pytest.fixture(scope="function", autouse=True)
     def browser_config(self):
         browser_name = "edge"
+        headless_mode=True
 
         if browser_name == "edge":
-            # srv = EgdeService(executable_path=r"C:\Mine\Software\msedgedriver.exe")
-            # self.driver = webdriver.Edge(service=srv)
             self.driver = webdriver.Edge()
         elif browser_name == "ff":
             self.driver = webdriver.Firefox()
         else:
-            # srv = ChromeService(executable_path=C:\Mine\Software\chromedriver.exe)
-            # self.driver = webdriver.Chrome(srv)
-            self.driver = webdriver.Chrome()
+            opt = webdriver.ChromeOptions()
+            if headless_mode:
+                opt.add_argument("--headless")
+
+            self.driver = webdriver.Chrome(options=opt)
 
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
